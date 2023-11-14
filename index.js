1c3219152563a9320eddafb6302360b64ae2c95b
Tayler <taylerblevins@gmail.com>
2023-11-14 13:33:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+      customerName = customerName.toUpperCase();
+      console.log(customerName);
+    } else {
+      console.log("Customer name is not a valid string.");
+    }
+  }
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'probley bob'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'sarah'
+}
